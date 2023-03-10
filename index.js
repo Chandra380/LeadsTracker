f6766ae67a5c78210da4e8f3c732b23c97042172
Chandra <cpdps1431@gmail.com>
2023-03-10 20:59:49 +0530

    added some lines

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ let ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value) //.value will give whatever is inside input element
+    renderLeads()
 })
 
-for(let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>"+myLeads[i]+"</li>"
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0; i < myLeads.length; i++){
+        // using string template
+        listItems+=
+        `<li>
+            <a href = '${myLeads[i]}' target = '_blank'>${myLeads[i]}</a>
+        </li>`
+        // create element
+        // set text content
+        // append to ul
+        // alternate method
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
+    inputEl.value = ""
 }
