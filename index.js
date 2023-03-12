edd9097f269d0ebb2a3efabe9374d639e7f03057
Chandra <cpdps1431@gmail.com>
2023-03-12 22:01:57 +0530

    added new lines

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let inputBtn = document.querySelector("#input-btn")
 let deleteBtn = document.querySelector("#delete-btn")
 let inputEl = document.querySelector("#input-el")
 let ulEl = document.querySelector("#ul-el")
+let tabBtn = document.querySelector("#tab-btn")
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if(leadsFromLocalStorage){
@@ -14,6 +15,16 @@ inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value) //.value will give whatever is inside input element
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
+})
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
 deleteBtn.addEventListener("dblclick", function(){
